9450883b474ce510b657b631369430eefbab1be7
dengfeng520 <deng_feng520@163.com>
2020-04-20 16:18:58 +0800

    update code

---
+++ src/model/homeController.js
@@ -0,0 +1,10 @@
+function clickJumpOverBtn() {
+
+    // window.location.href = "../views/HomePageView.html";
+    window.location.href = "../views/TSNBaseView/indexBase.html";
+}
+
+function load() {
+    console.log("test test test 002")
+
+}
